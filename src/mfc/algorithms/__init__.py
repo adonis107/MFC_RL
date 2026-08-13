03deda982e5eb727a705ea516005adb33d07c2a9
adonis <adonisjamal10@gmail.com>
2026-08-13 16:28:03 +0200

    added continuous mfreinforce

--- src/mfc/algorithms/__init__.py
+++ src/mfc/algorithms/__init__.py
@@ -1,5 +1,6 @@
 from .simplex_mfreinforce import SimplexPerturbedMFREINFORCE
 from .logits_mfreinforce import LogitsPerturbedMFREINFORCE
+from .continuous_mfreinforce import ContinuousTransportMFREINFORCE
 from .adaptive_simplex_mfreinforce import (
     AdaptiveSimplexControllerConfig,
     ConsistentAdaptiveSimplexMFREINFORCE,
